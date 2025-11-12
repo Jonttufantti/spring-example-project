fde09070456c420b330cee7b8d8793e0a5ef664b
Joona <joona.sandbacka@hotmail.fi>
2025-11-12 18:45:17 +0200

    again

--- src/main/java/com/devopsuh/dockerexample/controller/DefaultController.java
+++ src/main/java/com/devopsuh/dockerexample/controller/DefaultController.java
@@ -16,7 +16,7 @@ public class DefaultController {
 
     @RequestMapping(method = RequestMethod.POST)
     public String press(Model model) {
-        model.addAttribute("press", "Succes");
+        model.addAttribute("press", "Success");
         return "index";
     }
 }
